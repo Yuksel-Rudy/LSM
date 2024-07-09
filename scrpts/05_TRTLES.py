d96c5e2619d5df12934b033b7de9515839020c58
Alkarem <rudy.alkarem@nrel.gov>
2024-07-09 16:12:15 -0600

    for trtle

--- scrpts/05_TRTLES.py
+++ scrpts/05_TRTLES.py
@@ -2,20 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from model.lsm import LinearModel
 
-# Check input files R1Nx. Replace x with the xx or yy periodicity number required.
-Thrust = 1.95e6   # [N]
+# Check input files R0Lx. Replace x with the level number required.
+Thrust = 2.478302e6   # [N]
 delta_theta = 5  # [degree]
 
 # # Linear Model
-LSM_FILE = "inputs/lsm_input/R1Nxx2.yaml"
+LSM_FILE = "inputs/lsm_input/TRTLE(S).yaml"
 lsm = LinearModel(input_file=LSM_FILE)
 
+# linear
 x, y = lsm.get_array_watch_circle(force=Thrust,
                                   delta_theta=delta_theta,
                                   corrected=False,
-                                  magnify_watch_circle=5)
+                                  magnify_watch_circle=1)
 
-
+r = np.sqrt(np.array(x[0][0], dtype=float)**2 + np.array(y[0][0], dtype=float)**2)
+rmax = r.max()
 fig, ax = lsm.plot(labels=True)
 
 
