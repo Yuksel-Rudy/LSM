98d954e6dfc7a0e6bb7f6f5859c31d3438d4f588
Alkarem <rudy.alkarem@nrel.gov>
2024-07-17 11:03:58 -0600

    settings example 3 as the default for 07_ex2345_lsm

--- scrpts/07_ex2345_lsm.py
+++ scrpts/07_ex2345_lsm.py
@@ -4,8 +4,8 @@ from model.lsm2 import LinearModel
 
 Thrust = 1.95e6   # [N]
 delta_theta = 5  # [degree]
-input_file_BL = "inputs/lsm_input/ex5_BL.yaml"
-input_file_UC = "inputs/lsm_input/ex5_UC.yaml"
+input_file_BL = "inputs/lsm_input/ex3_BL.yaml"
+input_file_UC = "inputs/lsm_input/ex3_UC.yaml"
 
 BL = LinearModel(input_file_BL)
 UC = LinearModel(input_file_UC)
@@ -14,5 +14,5 @@ fig, ax = plt.subplots()
 BL.plot_wc(ax=ax, force=Thrust, delta_theta=delta_theta, color="blue", label="BL")
 plt.show()
 fig, ax = plt.subplots()
-UC.plot_wc(ax=ax, force=Thrust, delta_theta=delta_theta, color="red", label="UC")
+UC.plot_wc(ax=ax, force=Thrust, delta_theta=delta_theta, color="blue", label="UC")
 plt.show()
